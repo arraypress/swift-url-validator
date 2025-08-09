6ccd3681980e5920283d077a8e4d8b5d31eaad6a
David Sherlock <davidmichaelsherlock@icloud.com>
2025-08-09 13:47:16 +0300

    Initial Commit

---
+++ Tests/URLValidatorTests/URLValidatorTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import URLValidator
+
+final class URLValidatorTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
